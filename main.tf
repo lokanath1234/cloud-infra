47c52153420dc48114dfa6607596e2eafef24e78
Lokanath Sahoo <lsahoo@Lokanaths-MacBook-Pro.local>
2025-11-25 12:06:48 +0530

    initial terraform code

---
+++ main.tf
@@ -0,0 +1,36 @@
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "lk-bucket-terraform"
+    key    = "path/to/my/key"
+    region = "us-west-2"
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
