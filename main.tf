6427f7327deba60100956736aab380990784249d
Lokanath Sahoo <lsahoo@Lokanaths-MacBook-Pro.local>
2025-11-26 12:15:42 +0530

    spinning up multiple instances

--- main.tf
+++ main.tf
@@ -34,3 +34,21 @@ resource "aws_instance" "app_server" {
     Name = var.instance_name
   }
 }
+
+resource "aws_instance" "app_server2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_instance" "app_server3" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
